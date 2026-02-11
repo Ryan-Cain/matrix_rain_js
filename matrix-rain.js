c3c1ae503659d5c57c041c293481b393585c7128
Ryan Cain <84255629+Ryan-Cain@users.noreply.github.com>
2026-02-10 20:33:19 -0600

    randomized each drop speed

--- matrix-rain.js
+++ matrix-rain.js
@@ -2,21 +2,10 @@ const lettersToUse =
   "A日本語にほんごは「ひらがな」「カタカナ」「ローマ字（じ）」「漢字（かんじ）」を使（つか）って書（か）きます。わたしきんぎょたばことうきょうやそはちですワタシキンギョタバコトーキョーヤソハチデス";
 let dropPositions = [];
 
+// Interval to create drops
 setInterval(() => {
   createRainDrop();
-  const dropElements = document.getElementsByClassName("drop");
-  for (drop of dropElements) {
-    emptyEl = drop.getElementsByClassName("drop-empty")[0];
-    // emptyEl.innerText = emptyEl.innerText + "A";
-
-    dropHeadEl = drop.getElementsByClassName("drop-head")[0];
-    dropTailEl = drop.getElementsByClassName("drop-tail")[0];
-
-    dropTailEl.innerText = dropTailEl.innerText + dropHeadEl.innerText;
-    randomIndex = Math.floor(Math.random() * lettersToUse.length);
-    dropHeadEl.innerText = lettersToUse[randomIndex];
-  }
-}, 100);
+}, 50);
 
 // Add new drop to grid with random absolute position
 const createRainDrop = () => {
@@ -37,14 +26,27 @@ const createRainDrop = () => {
     newDrop.style.left = `${windowRand}px`;
     newDrop.innerHTML = `
     <div class="drop-real">
-      <span class="drop-empty dummy-text">D</span>
+      <span class="drop-empty dummy-text data-id="${windowRand}""></span>
       <span class="drop-tail drop-text"></span>
       <span class="drop-head drop-text">ん</span>
     </div>
   `;
     const grid = document.getElementById("grid");
     grid.appendChild(newDrop);
-    randomTimeout = Math.ceil(Math.random() * 6) * 1000;
+
+    randomNewLetterSpeed = Math.random() * (3 - 1) + 1;
+    setInterval(() => {
+      emptyEl = newDrop.getElementsByClassName("drop-empty")[0];
+
+      dropHeadEl = newDrop.getElementsByClassName("drop-head")[0];
+      dropTailEl = newDrop.getElementsByClassName("drop-tail")[0];
+
+      dropTailEl.innerText = dropTailEl.innerText + dropHeadEl.innerText;
+      randomIndex = Math.floor(Math.random() * lettersToUse.length);
+      dropHeadEl.innerText = lettersToUse[randomIndex];
+    }, randomNewLetterSpeed * 50);
+
+    randomTimeout = Math.random() * (4 - 2) + 2;
     setTimeout(() => {
       newDrop.classList.add("fade-out");
       dropPositions = dropPositions.filter(
@@ -53,8 +55,6 @@ const createRainDrop = () => {
       setTimeout(() => {
         newDrop.remove();
       }, 1500);
-    }, randomTimeout);
+    }, randomTimeout * 1000);
   }
 };
-
-createRainDrop();
